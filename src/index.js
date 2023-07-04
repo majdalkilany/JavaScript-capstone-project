693c0ea127a6c421afe83049696fbf5cdf960761
leloufadel <leloufadel@gmail.com>
2023-07-04 15:27:03 +0300

    linters fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.css';
+
 const fetchAPI = async () => {
   const baseURL = 'https://api.tvmaze.com/shows';
   const response = await fetch(baseURL);
   const data = await response.json();
   const shows = data.slice(0, 15); // Limit to shows
-}
+};
