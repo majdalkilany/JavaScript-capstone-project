50c5d4c4ca86e2c45cddf1aa667e5cf68517cbd3
leloufadel <leloufadel@gmail.com>
2023-07-05 21:25:59 +0300

    add event to the like button

--- src/index.js
+++ src/index.js
@@ -1,29 +1,82 @@
 import './style.css';
 
+const APP_ID = 'DUanzoHMk8l8HLimHh6p';
+let shows;
+
 const fetchAPI = async () => {
   const baseURL = 'https://api.tvmaze.com/shows';
   const response = await fetch(baseURL);
   const data = await response.json();
-  const shows = data.slice(0, 15); // Limit to shows
+  shows = data.slice(0, 15);
 
-  // Display the show list in the HTML
-  const showListContainer = document.getElementById('show-list');
+  // Fetch likes count for each item and combine with base API data
+  const likes = await getLikes();
   shows.forEach((show) => {
-    const showTitle = show.name;
-    const showImage = show.image.medium;
-
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <img src="${showImage}" alt="${showTitle}">
-      <h2 class="title">${showTitle}</h2>
-      <div class="interact">
-        <button class="comments">Comment</button>
-      </div>
-      <hr>
-    `;
-
-    showListContainer.appendChild(listItem);
+    const itemId = show.id;
+    const likeData = likes.find((item) => item.item_id === itemId);
+    show.likes = likeData ? likeData.likes : 0;
   });
 };
 
-fetchAPI().catch((error) => console.log(error));
+const getLikes = async () => {
+  const res = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`);
+  const data = await res.json();
+  return data;
+};
+
+const updateLikeCount = async (itemId) => {
+  try {
+    const fetchData = await fetch(
+      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          item_id: itemId,
+        }),
+      },
+    );
+    if (!fetchData.ok) throw new Error('Like cannot be added');
+    // Update the like count on the page
+    const likesCountElement = document.getElementById(`likes-count-${itemId}`);
+    if (likesCountElement) {
+      const likesCount = parseInt(likesCountElement.textContent, 10) + 1;
+      likesCountElement.textContent = likesCount;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+fetchAPI()
+  .then(() => {
+    // Display the show list in the HTML
+    const showListContainer = document.getElementById('show-list');
+    shows.forEach((show) => {
+      const showTitle = show.name;
+      const showImage = show.image.medium;
+      const itemId = show.id;
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <img src="${showImage}" alt="${showTitle}">
+        <h2 class="title">${showTitle}</h2>
+        <div class="interact">
+          <button class="comments">Comment</button>
+          <button class="likes" data-item-id="${itemId}"><i class="far fa-heart"></i></button>
+          <span class="likes-count" id="likes-count-${itemId}">${show.likes}</span>
+        </div>
+        <hr> 
+      `;
+      showListContainer.appendChild(listItem);
+
+      // Update like count when Like button is clicked
+      const likeButton = listItem.querySelector('.likes');
+      likeButton.addEventListener('click', () => {
+        const { itemId } = likeButton.dataset;
+        updateLikeCount(itemId);
+      });
+    });
+  })
+  .catch((err) => console.log(err));
