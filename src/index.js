cb517596cb3980db3400126cbe63a4b3f707f80d
leloufadel <leloufadel@gmail.com>
2023-07-05 17:47:59 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,37 +1,39 @@
-// import './style.css';
-// import { fetchAPI } from './modules/api.js';
-// import { displayData } from './modules/utils.js';
+//  import './style.css';
+// import { fetchAPI, updateLikeCount } from './modules/api.js';
 
-// const showListContainer = document.getElementById('show-list');
+// document.addEventListener('DOMContentLoaded', async () => {
+//   try {
+//     const shows = await fetchAPI();
 
-// fetchAPI()
-//   .then((shows) => {
+//     const showListContainer = document.getElementById('show-list');
 //     shows.forEach((show) => {
 //       const showTitle = show.name;
 //       const showImage = show.image.medium;
 //       const itemId = show.id;
+
 //       const listItem = document.createElement('li');
 //       listItem.innerHTML = `
 //         <img src="${showImage}" alt="${showTitle}">
 //         <h2 class="title">${showTitle}</h2>
 //         <div class="interact">
 //           <button class="comments">Comment</button>
-//           <button class="likes" onclick="updateLikeCount(${itemId})">Likes</button>
+//           <button class="likes">Likes</button>
 //           <span class="likes-count" id="likes-count-${itemId}">${show.likes}</span>
 //         </div>
 //         <hr>
 //       `;
 
 //       showListContainer.appendChild(listItem);
-// const likeButton = document.querySelector('.likes');
+
+//       const likeButton = listItem.querySelector('.likes');
 //       likeButton.addEventListener('click', () => {
 //         updateLikeCount(itemId);
 //       });
 //     });
-
-//     displayData(shows);
-//   })
-//   .catch((err) => console.log(err));
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 import './style.css';
 
