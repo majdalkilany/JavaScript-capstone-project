3f6ee27986e64143f6bb7ee507f7f7de92e5befc
leloufadel <leloufadel@gmail.com>
2023-07-05 12:59:41 +0300

    update the like item

--- src/index.js
+++ src/index.js
@@ -1,61 +1,29 @@
 import './style.css';
-const APP_ID= "DUanzoHMk8l8HLimHh6p";
-let shows;
-const fetchAPI = async () => {
-  const baseURL = 'https://api.tvmaze.com/shows';
-  const response = await fetch(baseURL);
-  const data = await response.json();
-  shows = data.slice(0, 15);
-};
+import { fetchAPI } from './modules/api.js';
+import { displayData } from './modules/utils.js';
+const showListContainer = document.getElementById('show-list');
 
-// function Get likes : 
+fetchAPI()
+  .then(shows => {
+    shows.forEach((show) => {
+      const showTitle = show.name;
+      const showImage = show.image.medium;
+      const itemId = show.id;
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <img src="${showImage}" alt="${showTitle}">
+        <h2 class="title">${showTitle}</h2>
+        <div class="interact">
+          <button class="comments">Comment</button>
+          <button class="likes" onclick="updateLikeCount(${itemId})">Likes</button>
+          <span class="likes-count" id="likes-count-${itemId}">${show.likes}</span>
+        </div>
+        <hr>
+      `;
 
-const getLikes = async() => {
-const res = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`);
-const data = await res.json();
-return data;
-console.log(data);
-}
-getLikes();
-const updateLikeCount = async (itemId) => {
-  try {
-    const fetchData = await fetch(
-      `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${APP_ID}/likes`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          item_id: itemId,
-        }),
-      },
-    );
-    if (!fetchData.ok) throw new Error('like cannot be add');
-    return fetchData;
-  } catch (e) {
-    return e;
-  }
-};
+      showListContainer.appendChild(listItem);
+    });
 
-fetchAPI().then(res => {
-      // Display the show list in the HTML
-  const showListContainer = document.getElementById('show-list');
-  shows.forEach((show) => {
-    const showTitle = show.name;
-    const showImage = show.image.medium;
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <img src="${showImage}" alt="${showTitle}">
-      <h2 class="title">${showTitle}</h2>
-      <div class="interact">
-        <button class="comments">Comment</button>
-        <button class="likes">Likes</button>
-      </div>
-      <hr>
-    `;
-
-    showListContainer.appendChild(listItem);
-  });
-    }).catch(err => console.log(err));
-
+    displayData(shows);
+  })
+  .catch(err => console.log(err));
