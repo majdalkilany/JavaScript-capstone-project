8ea8d3ee2b38242016707ee07bb6941832d224ec
majdalkilany <majdkilany92@gmail.com>
2023-07-05 23:14:26 +0300

    add popup and display the data inside it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './style.css';
+import { runModal } from './modules/modal.js';
 
 const fetchAPI = async () => {
   const baseURL = 'https://api.tvmaze.com/shows';
   const response = await fetch(baseURL);
   const data = await response.json();
-  const shows = data.slice(0, 15); // Limit to shows
+  const shows = data.slice(0, 15);
 
   // Display the show list in the HTML
   const showListContainer = document.getElementById('show-list');
@@ -21,9 +22,20 @@ const fetchAPI = async () => {
       </div>
       <hr>
     `;
+    const btn = listItem.querySelector('.myBtn');
+    btn.addEventListener('click', () => {
+      const modal = document.getElementById('myModal');
+      modal.innerHTML = ` <div class="modal-content">
+      <span class="close">&times;</span>
+      <h6>MovieShow</h6>
 
+    </div>`;
+      runModal(show);
+    });
     showListContainer.appendChild(listItem);
   });
 };
 
 fetchAPI().catch((error) => console.log(error));
+// const btn = document.querySelector('.myBtn');
+// btn.addEventListener('click', runModal);
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,25 +1,54 @@
 // Get the modal
-const modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-const btn = document.querySelector('myBtn');
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName('close')[0];
+export const runModal = ({
+  name,
+  summary,
+  language,
+  genres,
+  image: { original },
+}) => {
+  const modal = document.getElementById('myModal');
+  const modalContent = document.querySelector('.modal-content');
 
-// When the user clicks the button, open the modal
-btn.onclick = () => {
+  // Get the <span> element that closes the modal
+  const span = document.getElementsByClassName('close')[0];
+
+  // When the user clicks the button, open the modal
   modal.style.display = 'block';
-};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = () => {
-  modal.style.display = 'none';
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = (event) => {
-  if (event.target === modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = () => {
     modal.style.display = 'none';
-  }
+    modalContent.textContent = '';
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = (event) => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+      modalContent.textContent = '';
+    }
+  };
+
+  const descriptionContainer = document.createElement('div');
+  descriptionContainer.classList.add('modal-des');
+  modalContent.appendChild(descriptionContainer);
+
+  modalContent.innerHTML = `
+  <h6>MovieShow</h6>
+  <div class= "modal-content-container">
+  <img src="${original}" alt="${name}" class= "img-preview">
+  
+  <div class= "modal-description">
+  <h3> title : ${name}</h3>
+  <h4> language : ${language} </h4>
+  <h4> genres : ${genres[0]} ${genres[1]} </h4>
+  <p>  <span class="summary">Summary</span> : ${summary}  </p>
+  <br><br>
+  </div>
+  </div>
+ 
+  `;
 };
